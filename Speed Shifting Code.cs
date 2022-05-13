d84ea425c5c44fcc07e7770e407a409d1bbd1709
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-05-13 21:15:34 +0300

    Add files via upload

---
+++ Speed Shifting Code.cs
@@ -0,0 +1,19 @@
+Random rand = new Random();
+int yes = 0 ;
+const int iterations = 10000000;
+for (i = 0; i < iterations; i++);
+{
+    if (rand.Next(1,101) <=25
+    {
+        yes++;
+        PutItem(new Item1);
+    }
+}
+{
+    if (rand.Next(1,101) >=25)
+    {
+        no++;
+        DropItem(new Item2);
+    }
+}
+Console.Writeline((float)yes/iterations)
