df8acad0c10ed14760cbeb35deae14d3e4644f7d
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-06-30 00:21:19 +0300

    Add files via upload

---
+++ Torque Functioning of The Car.cs
@@ -0,0 +1,21 @@
+using UnityEngine ;
+using System.Collections ;
+class MainClass 
+set t = torque ;
+set n as force ;
+set m as lever arm ;
+{
+    private double n;
+
+    public double lever arm;
+    {
+        get {n * m = t }
+        get {
+            if (value <150) ;
+            DropItem Item 1 ;
+            if (value >150) ;
+            $"{nameof(value)} must be between 150 and 300.");
+        } torque = force * lever arm 
+        
+    }
+}
