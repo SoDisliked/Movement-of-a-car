54bb167b189d56e8ac84234d0347c96f5f9fa172
SoDisliked <90557319+SoDisliked@users.noreply.github.com>
2022-07-05 14:43:17 +0300

    Add files via upload

--- Torque Functioning of The Car.cs
+++ Torque Functioning of The Car.cs
@@ -19,6 +19,17 @@ set m as lever arm ;
         
     }
 }
+set piston area = a ;
+set D = cylinder diameter ;
+{
+    get {Math.Pi / 4 * D²}
+    get {
+        if (D value <40) ;
+        DropItem Item 2 ;
+        if (D value >40) ;
+        get a = $"{nameof(value)}" must be between 40 and 90" ; 
+    }
+}
 #pragma strict
 //engine powerband 
 set motor speed = rpm ;
@@ -33,3 +44,28 @@ set pairs of poles = 10 ;
     } set //maximum Engine Torque 
     var maxTorque = 400 ; 
 }
+    set power = kW ;
+    set Pm = mean effective pressure ; 
+    set L = length of the stroke ;
+    set A = area of piston (m²) ;
+    set N = rotational speed of rpm ;
+    get {((Pm L*A*N)n * 100)/60}
+    {
+        if (kW < 60.000) ;
+        put new BMEP ;
+        if (kW > 60.000) ;
+        put new IMEP ;
+    } 
+ {  set BMEP {public.Fraction (int numerator, int denumerator)} 
+    
+        set int numerator = kW * 60 ;
+
+{
+    set int denominator = L * A * (N/n) * cylinders * 100
+        if (denominator == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.", nameof(denominator));
+        }
+    }
+}
+
